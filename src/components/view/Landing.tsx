081d68a2328cbf5af62e29cb148dba8636f1d559
Jason Warren <contact.jwarren@gmail.com>
2024-10-16 15:00:38 +0100

    All sorts of tailwindy bullshit

--- src/components/view/Landing.tsx
+++ src/components/view/Landing.tsx
@@ -6,10 +6,12 @@ export function LandingView() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <p className="text-xl mb-8 text-center">Your one-stop shop for all your leisure and hobby needs</p>
+      <p className="text-xl mb-8 text-center">
+				Your one-stop shop for all your leisure and hobby needs
+			</p>
       <button
         onClick={() => setView('search')}
-        className="bg-blue-500 text-white px-6 py-2 rounded-full text-lg hover:bg-blue-600 transition-colors"
+        className="px-6 py-2 rounded-full text-lg transition-colors bg-primary-0 text-primary-5 hover:opacity-90"
       >
         Start Shopping
       </button>
