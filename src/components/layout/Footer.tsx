7efbe90faa17d457e6286300bc4ab149553121ea
Jason Warren <contact.jwarren@gmail.com>
2024-10-22 16:05:13 +0100

    feat: :passport_control: visually register whether the user is logged in

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useUser } from '../../context/User'
 
 function Footer() {
+	const { isLoggedIn, setIsLoggedIn } = useUser();
+
   return (
     <footer className='bg-primary-2 w-full'>
-      <div className='container mx-auto px-4 py-4'>
-        <p className='text-primary-0 text-left'>
-          Don't steal our shit
-        </p>
-      </div>
+			<div className="flex container justify-between">
+				<div>
+					{isLoggedIn ? (
+						<button className="button-bonbon" onClick={
+							() => setIsLoggedIn(false)}
+						>Logout</button>
+					) : (
+						<button className="button-bonbon" onClick={
+							() => setIsLoggedIn(true)}
+						>Login</button>
+					)}
+				</div>
+
+				<div className='container mx-auto px-4 py-4'>
+					<p className='text-primary-0 text-left'>
+						Don't steal our shit
+					</p>
+				</div>
+			</div>
     </footer>
   )
 }
