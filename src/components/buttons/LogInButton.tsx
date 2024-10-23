f12dd066aecb7616d51de742dd6f6fc142844fa9
Jason Warren <contact.jwarren@gmail.com>
2024-10-23 14:51:23 +0100

    feat: :construction: navigate to login view when pressing login button

--- src/components/buttons/LogInButton.tsx
+++ src/components/buttons/LogInButton.tsx
@@ -1,15 +1,18 @@
-import { LogIn } from "lucide-react"
-import { useUser } from '../../context/User'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/Store';
+import { useUser } from '../../context/User';
+import { LogInIcon } from "lucide-react"
 
 function LogInButton() {
+	const { setView } = useContext(StoreContext);
 	const { setIsLoggedIn } = useUser();
 
 	return (
 		<button
 		className='mr-2 text-primary-A hover:text-primary-0 transition-colors'
-		onClick={() => setIsLoggedIn(true)}
+		/* onClick={() => setIsLoggedIn(true)} */ onClick={() => setView('login')}
 		>
-			<LogIn className="h-6 w-6" />
+			<LogInIcon className="h-6 w-6" />
 		</button>
 	)
 }
