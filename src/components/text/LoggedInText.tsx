4b7e9c2a219eb1c4bab1ef5c3a09357170f33f57
Jason Warren <contact.jwarren@gmail.com>
2024-10-31 12:11:45 +0000

    feat(text): :speech_balloon: make LoggedInText more descriptive

--- src/components/text/LoggedInText.tsx
+++ src/components/text/LoggedInText.tsx
@@ -1,5 +1,11 @@
 function LoggedInText() {
-	return( <p>"Already Logged In"</p> );
+	return(
+		<>
+			<h2>Already Logged In</h2>
+			<p>Sorry, we've brought you to a page you shouldn't be able to access.</p>
+			<p>If you're sure you're not logged in, please refresh your browser.</p>
+		</>
+	);
 }
 
 export { LoggedInText };
